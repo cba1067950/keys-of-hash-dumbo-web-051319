b21842cb23179af04d8a5fbb31e272f89acc4de1
Joe Jamsky <cba1067950@yahoo.com>
2019-05-09 03:00:00 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,12 +3,21 @@ class Hash
   def keys_of(*arguments)
     # code goes here
     argsArray = []
+<<<<<<< HEAD
     
     self.each do |key, value|
       if arguments.include?(value)
         argsArray << key
       end
     end
+=======
+    a = *arguments
+  
+    a.each do |items|
+      argsArray << items
+    end
+    #puts argsArray
+>>>>>>> f285bbe05b5b0a4e54086dba66e703bde5449c97
     argsArray
   end
 end
